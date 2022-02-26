7a2d265a3cde4a0f14e90fcac786d14e607c3cad
Kyle Reczek <kyle@banyango.com>
2022-02-26 14:57:37 -0700

    Trigger storybook workflow

--- packages/core/src/lib/checkbox/checkbox.spec.tsx
+++ packages/core/src/lib/checkbox/checkbox.spec.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 
 import Checkbox from './checkbox';
 
+
 describe('Checkbox', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Checkbox />);
